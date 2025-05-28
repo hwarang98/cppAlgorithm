72d15f1b084bb3786585bad1677296aab1e9a961
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2025-05-28 09:34:20 +0900

    Create numberOfDigitsSum.cpp

---
+++ LV0/numberOfDigitsSum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string numString = to_string(n); 
+    
+    for(int i = 0; i < numString.size(); i++){
+        answer += (numString[i]-'0');
+    }
+
+    return answer;
+}
