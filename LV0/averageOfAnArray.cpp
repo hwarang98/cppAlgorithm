078f8015114138f174e2c98ab18fd8c4c975028b
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2025-05-29 21:03:36 +0900

    Update averageOfAnArray.cpp

--- LV0/averageOfAnArray.cpp
+++ LV0/averageOfAnArray.cpp
@@ -1,12 +1,15 @@
 #include <string>
 #include <vector>
+#include <numeric>   // accumulate, plus
 
 using namespace std;
 
 double solution(vector<int> numbers) {
-    double answer = 0;
-    for(int i = 0; i < numbers.size(); i++){
-        answer += numbers[i];
-    }
-    return answer / numbers.size();
+
+    // 1. 벡터 numbers의 합을 구함
+    int sum = accumulate(begin(numbers), end(numbers), 0);
+
+    // 2. 평균 계산 (sum을 double로 형변환한 뒤 크기로 나눔)
+    return (double)sum / numbers.size();
+
 }
