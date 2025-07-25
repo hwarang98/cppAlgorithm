56d2e71df7606567068807390e9465a2dff6f3cd
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2025-07-25 10:14:21 +0900

    Create 바탕화면정리.cpp

---
+++ LV1/바탕화면정리.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<string> wallpaper) {
+    vector<int> answer;
+    vector<int> x, y;
+    for (int i = 0; i < wallpaper.size(); i++) {
+        for (int j = 0; j < wallpaper[i].size(); j++) {
+            if (wallpaper[i][j] == '#') {
+                x.push_back(i);
+                y.push_back(j);
+            }
+        }
+    }
+    answer.push_back(*min_element(x.begin(), x.end()));
+    answer.push_back(*min_element(y.begin(), y.end()));
+    answer.push_back(*max_element(x.begin(), x.end()) + 1);
+    answer.push_back(*max_element(y.begin(), y.end()) + 1);
+    return answer;
+}
