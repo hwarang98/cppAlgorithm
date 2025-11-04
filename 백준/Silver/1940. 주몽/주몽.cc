8adc0c08a2001e5212f5da86b419a956843fb15d
LeeHwaRang <84739055+hwarang98@users.noreply.github.com>
2025-11-05 00:50:35 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,37 +1,39 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int materialTotalCount, numberOfArmor, materialNumber, result;
-vector<int> materialArray;
-
-
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
-    cout.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(NULL);
 
+    int materialTotalCount, numberOfArmor;
     cin >> materialTotalCount >> numberOfArmor;
 
-
+    vector<int> materialArray(materialTotalCount);
     for (int i = 0; i < materialTotalCount; ++i)
-    {
-        cin >> materialNumber;
-        materialArray.emplace_back(materialNumber);
-    }
-    
-    for (int i = 0; i < materialArray.size(); i++)
-    {
-        for (int j = i + 1; j < materialArray.size(); j++)
-        {
-            if (materialArray[i] + materialArray[j] == numberOfArmor)
-            {
-                result++;
-            }
+        cin >> materialArray[i];
+
+    sort(materialArray.begin(), materialArray.end());
+
+    int left = 0;
+    int right = materialTotalCount - 1;
+    int result = 0;
+
+    while (left < right) {
+        int sum = materialArray[left] + materialArray[right];
+
+        if (sum == numberOfArmor) {
+            result++;
+            left++;
+            right--;
+        }
+        else if (sum < numberOfArmor) {
+            left++;  // 합을 키움
+        }
+        else {
+            right--; // 합을 줄임
         }
     }
 
     cout << result << "\n";
-    
     return 0;
 }
